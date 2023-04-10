7bdf758d7e3bfe1aa5200c86e06c2816f3a3ee4e
Ohara Kerusauskas Rayel <oharakr@gmail.com>
2023-04-10 15:45:35 -0300

    Revert wrong Code Cleanup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -161,6 +161,14 @@ const osMessageQueueAttr_t ModemSendQueue_attributes = {
   .mq_mem = &ModemSendQueueBuffer,
   .mq_size = sizeof(ModemSendQueueBuffer)
 };
+/* Definitions for PeriodicSendTimer */
+osTimerId_t PeriodicSendTimerHandle;
+osStaticTimerDef_t PeriodicSendTimerControlBlock;
+const osTimerAttr_t PeriodicSendTimer_attributes = {
+  .name = "PeriodicSendTimer",
+  .cb_mem = &PeriodicSendTimerControlBlock,
+  .cb_size = sizeof(PeriodicSendTimerControlBlock),
+};
 /* Definitions for ModemLedTimer */
 osTimerId_t ModemLedTimerHandle;
 osStaticTimerDef_t ModemLedTimerControlBlock;
@@ -346,6 +354,9 @@ int main(void)
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* Create the timer(s) */
+  /* creation of PeriodicSendTimer */
+  PeriodicSendTimerHandle = osTimerNew(PeriodicSendTimerCallback, osTimerPeriodic, NULL, &PeriodicSendTimer_attributes);
+
   /* creation of ModemLedTimer */
   ModemLedTimerHandle = osTimerNew(ModemLedCallback, osTimerPeriodic, NULL, &ModemLedTimer_attributes);
 
